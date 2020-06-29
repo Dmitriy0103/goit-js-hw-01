537d32aac520dccc04b8e96b4426a38cdcb6c9b7
Dmitriy <GarantP@i.ua>
2020-06-29 19:05:10 +0300

    module 2

---
+++ module 2/js2/task_number_3.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const findLongestWord = function(string){
+    const words = string.split(' ');
+    let longestword = words[0];
+
+    for (const word of words) {
+        if(word.length > longestword.length) {
+            longestword = word;
+        }
+    }
+
+    return longestword;
+};
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('Google do a roll'));
+console.log(findLongestWord('May the force be with you'));
+
+
+
+
+
+
+
+
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+// const findLongestWord = function(string) {
+//   // твой код
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
