8fac7ff9143d65da9562fcb91612a2914ac7a8ce
Dmitriy <GarantP@i.ua>
2020-09-21 21:33:48 +0300

    re module 5

--- module-5/js5/homework1.js
+++ module-5/js5/homework1.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const Account = function(login,email){
+const Account = function({login,email}){
     this.login = login;
     this.email = email;
-}
+};
 
-Account.prototype.getInfo = function(){
-    console.log(`${this.login},${this.emai}`);
-}
+Account.prototype.getInfo = function() {
+    console.log(`Login: ${this.login}, Email: ${this.email}`);
+};
 
 console.log(Account.prototype.getInfo); // function
 
